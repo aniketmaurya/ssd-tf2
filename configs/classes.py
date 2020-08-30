a5b33f6f3d02114684ddef1bfce5bb22426abd37
Aniket Maurya <aniket.maurya@coviam.com>
2020-08-30 15:04:34 +0530

    dataset classes

---
+++ configs/classes.py
@@ -0,0 +1,12 @@
+from easydict import EasyDict
+
+classes = EasyDict()
+
+
+classes.voc = [
+    'aeroplane', 'bicycle', 'bird', 'boat',
+    'bottle', 'bus', 'car', 'cat', 'chair',
+    'cow', 'diningtable', 'dog', 'horse',
+    'motorbike', 'person', 'pottedplant',
+    'sheep', 'sofa', 'train', 'tvmonitor'
+]
